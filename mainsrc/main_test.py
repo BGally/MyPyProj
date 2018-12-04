6075bd42d11ce995604475bf30d5bca1e099a9c5
BGallagher <bgallagher@guidewire.com>
2018-12-04 14:33:25 +1300

    Initial commit

---
+++ mainsrc/main_test.py
@@ -0,0 +1,12 @@
+# Test
+
+import main
+
+
+def test_index():
+    main.app.testing = True
+    client = main.app.test_client()
+
+    r = client.get('/')
+    assert r.status_code == 200
+    assert 'Hola Diana y Brian!' in r.data.decode('utf-8')
